011723fc2632169602f130920bbafc5d2c136fee
gustsu <justintew@eagles.ewu.edu>
2016-06-23 13:34:24 -0700

    reorganized for readability

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
+//get the current year and put it in the footer
+var year = new Date();
+$("#year").text(year.getFullYear());
+
+
+//parallax scrolling effect using the plug in stellar.js
+$(window).stellar();
+
+
+
+//dropdown Functionality
+$("#dropDown").hide();
+
+$("#ddTriger").mouseover(function(){
+    $("#dropDown").slideDown(500);
+});
+
+$("#dropDown").mouseleave(function(){
+    $("#dropDown").slideUp(500);
+});
+
+
+
 //accordion text
 $('.ui.accordion').accordion();
+
+
 
 //smooth scrolling credit to css-tricks.com
 $(function() {
@@ -15,24 +40,4 @@ $(function() {
       }
     }
   });
-});
-
-
-//get the current year and put it in the footer
-var year = new Date();
-$("#year").text(year.getFullYear());
-
-
-//parallax scrolling effect using the plug in stellar.js
-$(window).stellar();
-
-//dropdown Functionality
-$("#dropDown").hide();
-
-$("#ddTriger").mouseover(function(){
-    $("#dropDown").slideDown(500);
-});
-
-$("#dropDown").mouseleave(function(){
-    $("#dropDown").slideUp(500);
 });
